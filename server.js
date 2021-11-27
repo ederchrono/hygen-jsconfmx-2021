853c79926aa5622d1ad357f86c781e0a967b805f
Eder Díaz <ederchrono@gmail.com>
2021-11-27 16:16:49 -0600

    :fire: remove commented code

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const port = 3000
 const APIController = require('./api/controller')
 
 app.use(express.static('public'))
-// app.get('/', (req, res) => {
-  // res.send('Hello World!')
-// })
+
 app.use('/api', APIController)
 
 app.listen(port, () => {
